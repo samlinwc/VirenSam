6050a1b73f6f215c5d68d4b9cd5aa954d6dd9659
Sam Lin <linweichensam0629@gmail.com>
2016-02-11 11:24:20 +0000

    lol

--- virensam/src/virensam/Camera.java
+++ virensam/src/virensam/Camera.java
@@ -1,5 +1,12 @@
 package virensam;
 
+import javax.swing.*;
+
 public class Camera {
+	public static void main (String args[]){
+		JFrame myFrame = new JFrame ("Matthew Damon on Mars");
+		myFrame.setSize(300, 600);
+		
+	}
 
 }
