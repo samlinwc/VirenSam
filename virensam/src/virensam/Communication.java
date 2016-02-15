384b25ea0d0c104f84a4cd9ffeb030adb4fb2da6
Sam Lin <linweichensam0629@gmail.com>
2016-02-15 04:09:48 +0000

    Shifts slider with int value between 0-15

--- virensam/src/virensam/Communication.java
+++ virensam/src/virensam/Communication.java
@@ -1,5 +1,77 @@
 package virensam;
 
-public class Communication {
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Hashtable;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+
+
+public class Communication extends JFrame implements ActionListener{
+	
+	JTextField jtfinput;
+	JButton jbsend;
+	JSlider Camera;
+	
+	public Communication(){
+		super ("Matthew Damon on Mars");
+		
+		Camera = new JSlider(0,15,0);
+		jtfinput = new JTextField();
+		jbsend = new JButton("Send");
+		
+		jbsend.addActionListener(this);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Camera.setPaintLabels(true);
+		Camera.setPaintTicks(true);
+		Camera.setMajorTickSpacing(1);
+		
+		Hashtable<Integer, JLabel> table = new Hashtable<Integer,JLabel>();
+		
+		table.put (0,new JLabel("0"));
+		table.put (1,new JLabel("1"));
+		table.put (2,new JLabel("2"));
+		table.put(3, new JLabel("3"));
+ 		table.put(4, new JLabel("4"));
+ 		table.put(5, new JLabel("5"));
+ 		table.put(6, new JLabel("6"));
+ 		table.put(7, new JLabel("7"));
+ 		table.put(8, new JLabel("8"));
+ 		table.put(9, new JLabel("9"));
+ 		table.put(10, new JLabel("A"));
+ 		table.put(11, new JLabel("B"));
+ 		table.put(12, new JLabel("C"));
+ 		table.put(13, new JLabel("D"));
+ 		table.put(14, new JLabel("E"));
+ 		table.put(15, new JLabel("F"));
+ 		Camera.setLabelTable(table);
+	
+		this.add(jtfinput,BorderLayout.CENTER);
+ 		this.add(Camera, BorderLayout.NORTH);
+ 		this.add(jbsend,BorderLayout.SOUTH);
+	}
+
+	public static void main(String args[]) {
+		JFrame Communication = new Communication();
+		Communication.setVisible(true);
+		Communication.setSize(400, 500);
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String input = jtfinput.getText();
+		int iinput = Integer.parseInt(input);
+		
+		Camera.setValue(iinput);
+	}
+	
+
+	
 }
